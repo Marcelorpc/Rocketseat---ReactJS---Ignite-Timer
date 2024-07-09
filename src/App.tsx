5d5be30a7a8ba9aaa9c1df5809bfc4b4c6062086
Marcelo Paiva <marcelo.paiva@argobr.com>
2024-07-09 06:20:43 -0300

    implementação de tema personalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import { ThemeProvider } from "styled-components"
 import Button from "./components/Button"
 import { GlobalStyle } from "./styles/global"
+import { defaultTheme } from "./styles/default"
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button backgroundColor="primary" />
       <Button backgroundColor="danger" />
 
       <GlobalStyle />
-    </>
+    </ThemeProvider>
   )
 }
 
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,5 +5,16 @@ export const GlobalStyle = createGlobalStyle`
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  font-size: 60px;
+ }
+
+body {
+  background-color: ${props => props.theme["gray-900"]};
+}
+
+ body, input, textarea, button {
+  font-family: 'Roboto', sans-serif;
+  font-weight: 400;
+  font-size: 1rem;
  }
 `
